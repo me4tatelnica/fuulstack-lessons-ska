744f4f3a0ee3104d237e58609128f1c23a181f8d
me4tatelnica <mamiha2010@gmail.com>
2025-07-06 11:31:34 +0300

    Пройден урок 11.13. "Выделение компонентов

--- 007-chapter/src/Layout/Homepage/HomePage.jsx
+++ 007-chapter/src/Layout/Homepage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import HeaderItem from './components/Headeritem';
+
 import ContentItem from './components/ContentItem';
 import FooterItem from './components/FooterItem';
+import HeaderItem from './components/Headeritem';
 
 const HomePage = () => {
   return (
     <div  className="container col-3">
+
       <HeaderItem />
       <ContentItem />
       <FooterItem />
--- 007-chapter/src/index.js
+++ 007-chapter/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
+import HomePage from './Layout/Homepage/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
-
-
 root.render(
-
+  <HomePage />
 );
 
 
