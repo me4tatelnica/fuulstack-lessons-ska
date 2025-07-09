5f6358728883e3d42b7d0871b8d31d6b65d7664c
me4tatelnica <mamiha2010@gmail.com>
2025-07-09 07:29:16 +0300

    Пройден урок: "Косметические правки"

--- 007-chapter/src/Layout/Homepage/components/ContentItem.jsx
+++ 007-chapter/src/Layout/Homepage/components/ContentItem.jsx
@@ -4,15 +4,15 @@ const ContentItem =(props) => {
   return (
     <div>
     {props.setOfLabels.map((item, index) => (
-      <div className="card">
+      <div key={item.id} className="card">
         <img
           src={`https://placebear.com/${200 + index}/100`}
           className="card-img-top"
           alt="..."
         />
         <div className="card-body">
-          <h5 className="card-title">{item.title}</h5>
-          <p className="card-text">{item.body}</p>
+          <h5 className="card-title">{item.title.slice(0, 30)}</h5>
+          <p className="card-text">{item.body.slice(0, 70)}</p>
         </div>
       </div>
     ))}
