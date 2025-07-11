107ffdbd221a33aa574a8ed8efeb00d7f8556f20
me4tatelnica <mamiha2010@gmail.com>
2025-07-11 07:55:52 +0300

    Пройдено два урока: "Переменная", "Базовая арифметика"

---
+++ 008-chapter/002-variables/Program.cs
@@ -0,0 +1,23 @@
+﻿// Строгая типизация, объявление переменных
+int age; // Объявление переменной типа int с именем age
+double salary; // Объявление переменной типа double с именем salary
+string name; // Объявление переменной типа string с именем name
+
+// Присваивание значений
+age = 30; // Присваивание значения 30 переменной age
+salary = 0002500.50000; // Присваивание значения 2500.50 переменной salary
+name = "John"; // Присваивание значения "John" переменной name
+
+// Инициализация переменных
+string productName = "Ноутбук"; // Двойные кавычки для string
+char c = 'A';                   // Одинарные кавычки для char
+double price = 1200.99;
+int quantityInStock = 50;
+productName = "Телефон";
+
+// Чувствительность к регистру
+// productName - не равно ProductName
+
+Console.WriteLine("Какой-то текст");
+Console.WriteLine("Наименование товара " + productName);
+Console.WriteLine("Ценник " + price);
