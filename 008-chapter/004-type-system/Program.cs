3ba20b7bbb6e012c2f1385c98543794caa560a38
me4tatelnica <mamiha2010@gmail.com>
2025-07-11 08:21:38 +0300

    Пройден урок: "Система типов"

---
+++ 008-chapter/004-type-system/Program.cs
@@ -0,0 +1,39 @@
+﻿// Пример работы с числовыми типами целых чисел
+int integerNumber = 42;
+Console.WriteLine("Целое число: " + integerNumber);
+
+// Пример работы с числовым типом с плавающей точкой
+double floatingPointNumber = 3.14;
+Console.WriteLine("Число с плавающей точкой: " + floatingPointNumber);
+
+// Пример работы с логическим типом
+bool booleanValue = true;
+Console.WriteLine("Логическое значение: " + booleanValue);
+
+// Пример работы с символьным типом
+char character = 'A';
+Console.WriteLine("Символ: " + character);
+
+string name = "John Doe";
+
+// Особый тип данных - object
+object anyType = "Это строка";
+
+Console.WriteLine("Object хранит: " + anyType);
+anyType = 10;
+Console.WriteLine("Теперь object хранит: " + anyType);
+
+// Пример работы с перечислениями
+Week today = Week.Monday;
+Console.WriteLine("День недели: " + today);
+enum Week
+{
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
