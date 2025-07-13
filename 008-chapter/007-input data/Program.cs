4af8228443a4b954990d317abead878151160cd8
me4tatelnica <mamiha2010@gmail.com>
2025-07-13 13:51:25 +0300

    пройден урок: "Получение данных"

---
+++ 008-chapter/007-input data/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пример ввода строки с консоли
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+// Вывод приветствия с именем пользователя
+Console.WriteLine($"Привет, {name}!");
+Console.WriteLine("Привет, " + name + "!");
+
+// Пример ввода одного символа с консоли
+Console.Write("Нажмите любую клавишу, чтобы продолжить...");
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+// Вывод информации о нажатой клавише
+Console.WriteLine($"\nВы нажали клавишу {keyInfo.KeyChar}.");
+
+// Пример обработки числового ввода
+Console.Write("Введите ваш возраст: ");
+string inputAge = Console.ReadLine();
+
+// Попытка преобразовать строку в число
+int age = default;
+bool parseResult = int.TryParse(inputAge, out age);
+// Вывод возраста, если ввод корректен
+Console.WriteLine($"Получилось ли преобразовать: {parseResult}");
+Console.WriteLine($"Ваш возраст: {age} лет.");
