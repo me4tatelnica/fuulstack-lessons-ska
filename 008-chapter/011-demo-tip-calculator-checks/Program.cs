7eafa35b9eb6c79ba02b1e2e393be1675d55511b
me4tatelnica <mamiha2010@gmail.com>
2025-07-16 08:45:32 +0300

    Выполнено две практические работы

---
+++ 008-chapter/011-demo-tip-calculator-checks/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите сумму счёта:");
+double billAmount;
+
+if (!double.TryParse(Console.ReadLine(), out billAmount) || billAmount <= 0)
+{
+    Console.WriteLine("Ошибка ввода данных");
+}
+
+Console.WriteLine("Введите процент чаевых:");
+double tipPercentage;
+if (!double.TryParse(Console.ReadLine(), out tipPercentage)
+    || tipPercentage < 0)
+{
+  Console.WriteLine("Ошибка ввода данных");
+}
+
+Console.WriteLine("Введите количество человек:");
+int numberOfPeople;
+if (!int.TryParse(Console.ReadLine(), out numberOfPeople)
+    || numberOfPeople <= 0)
+{
+  Console.WriteLine("Ошибка ввода данных");
+}
+
+double tipAmount = billAmount * tipPercentage / 100;
+double totalAmount = billAmount + tipAmount;
+double perPersonAmount = totalAmount / numberOfPeople;
+
+Console.WriteLine($"Общий счёт: {totalAmount}");
+Console.WriteLine($"Процент чаевых: {tipPercentage}%");
+Console.WriteLine($"С каждого: {perPersonAmount}");
---
+++ 008-chapter/012-demo-text-questionnaire/Program.cs
@@ -0,0 +1,29 @@
+﻿int score = 0;
+
+string question = "Переведи слово 'table'? [стол, табло]";
+Console.WriteLine(question);
+string answer = Console.ReadLine();
+if (answer == "стол")
+{
+    score += 10;
+    Console.WriteLine("Молодец!");
+}
+else
+{
+    Console.WriteLine("Открой словарь!");
+}
+
+question = "Переведи слово 'true'? [истинный, ложный]";
+Console.WriteLine(question);
+answer = Console.ReadLine();
+if (answer == "истинный")
+{
+    score += 10;
+    Console.WriteLine("Молодец!");
+}
+else
+{
+    Console.WriteLine("Открой словарь!");
+}
+
+Console.WriteLine($"Счёт: {score}");
