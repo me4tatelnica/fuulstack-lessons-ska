f9417af9ff8b83c198362ee104c17ab06bfca0ee
me4tatelnica <mamiha2010@gmail.com>
2025-07-30 07:30:36 +0300

    Пройдены строки, выполнена практическая работа

---
+++ 008-chapter/013-string/Program.cs
@@ -0,0 +1,64 @@
+﻿// Пример использования метода Concat
+string hello = "Hello, ";
+string world = "world!";
+string combined = string.Concat(hello, world);
+// Выведет: Hello, world!
+Console.WriteLine(combined);
+
+// Пример использования метода Contains
+bool containsWorld = combined.Contains("world");
+// Выведет: true
+Console.WriteLine($"Строка содержит 'world': {containsWorld}");
+
+// Пример использования метода IndexOf
+//                        0123456    789...
+combined = string.Concat("Hello, ", "world!");
+int indexOfW = combined.IndexOf('w');
+// Выведет: 7
+Console.WriteLine($"Индекс буквы 'w': {indexOfW}");
+
+// Пример использования метода Substring
+string sub = combined.Substring(7);
+// Выведет: world!
+Console.WriteLine(sub);
+
+// Пример использования метода Replace
+// combined = combined + "world " + "world123" + "world1 !";
+string replaced = combined.Replace("world", "C#");
+// Выведет: Hello, C#!
+Console.WriteLine(replaced);
+
+// Пример использования метода Split
+var parts = "one,two,three".Split(',');
+
+// Выведет: one
+Console.WriteLine($"Первая часть: {parts[0]}");
+
+// Пример использования методов ToUpper и ToLower
+string upper = combined.ToUpper();
+// Выведет: HELLO, WORLD!
+Console.WriteLine(upper);
+string lower = combined.ToLower();
+// Выведет: hello, world!
+Console.WriteLine(lower);
+
+// Пример использования метода Trim
+string trimmed = "  Hello, world!  ".Trim();
+// Выведет: 'Hello, world!'
+Console.WriteLine($"Обрезанная строка: '{trimmed}'");
+
+// Пример получения длины строки
+int length = combined.Length;
+// Выведет: 13
+Console.WriteLine($"Длина строки: {length}");
+
+// Пример сравнения строк
+
+bool areEqual = string.Equals("Hello", "hello", StringComparison.OrdinalIgnoreCase);
+// Выведет: false
+Console.WriteLine($"Строки равны (без учета регистра): {areEqual}");
+
+// 0 
+string c = Convert.ToString(combined[1]);
+c = combined[1].ToString();
+
---
+++ 008-chapter/014-concatenation-string/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+// Пример использования оператора +
+string hello = "Hello, ";
+string world = "world!";
+string greeting1 = hello + world;
+// Выведет: Hello, world!
+Console.WriteLine(greeting1);
+
+// Пример использования метода String.Concat
+string greeting2 = String.Concat("Hello, ", "world", "!");
+// Выведет: Hello, world!
+Console.WriteLine(greeting2);
+
+// Пример использования метода String.Join
+string[] words = { "Hello", "world" };
+string greeting3 = String.Join(", ", words) + "!";
+// Выведет: Hello, world!
+Console.WriteLine(greeting3);
+
+// Пример использования строковой интерполяции
+string greeting4 = $"Hello, {world}";
+// greeting4[3] = '!';
+// Выведет: Hello, world!
+Console.WriteLine(greeting4);
+
+// Пример использования StringBuilder для конкатенации строк
+
+StringBuilder sb = new();
+sb.Append("Hello, ");
+sb.Append("world!");
+sb.Append(123);
+sb[1] = '!';
+string greeting5 = sb.ToString();
+// Выведет: Hello, world!
+Console.WriteLine(greeting5);
+
+// Пример интернирования строк
+string a = "Hello, world!";
+string b = "Hello, world!";
+// true, так как строки интернированы
+bool areSame1 = ReferenceEquals(a, b);
+Console.WriteLine($"Строки a и b интернированы: {areSame1}");
+
+string s1 = "Hello,";
+string s2 = " world!";
+string a2 = s1 + s2;
+string b2 = "Hello, world!";
+// false, так как строки не интернированы после конкатенации
+bool areSame2 = ReferenceEquals(a2, b2);
+Console.WriteLine($"Строки a2 и b2 интернированы: {areSame2}");
+
+// Пример ручного интернирования строк
+string nonInterned = String.Intern(a2);
+// теперь true, так как строка a2 была интернирована
+bool areSame3 = ReferenceEquals(nonInterned, b2);
+Console.WriteLine($"Строки nonInterned и b2 интернированы после ручного интернирования: {areSame3}");
