fc7080d31163f334d0b51239ad252dc35f6e90e9
me4tatelnica <mamiha2010@gmail.com>
2025-08-22 10:57:47 +0300

    Урок про контроллеры. Попытка 1

---
+++ ContactApp/Api/Controller/TestController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+  // app.MapGet("/test", () => "Hello, World!");
+  // "http://localhost:5000"/api/Test/test
+  [HttpGet("test")]
+  public string GetHelloWorldText()
+  {
+    return "Hello world!";
+  }
+}
