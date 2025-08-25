957dc6a8862a443a2a063aed26c4b91c4221e176
me4tatelnica <mamiha2010@gmail.com>
2025-08-25 12:39:20 +0300

    Пройден урок "Хранилище данных"

--- ContactApp/Api/Program.cs
+++ ContactApp/Api/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddSingleton<DataContext>();
 
 var app = builder.Build();
 app.UseSwagger();
