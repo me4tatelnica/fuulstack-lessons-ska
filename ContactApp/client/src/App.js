40c77ab8a76c6c1bc9bd4700be47541e318b58bf
me4tatelnica <mamiha2010@gmail.com>
2025-08-31 10:38:39 +0300

    Пройден урок "Подготовка представления"

--- ContactApp/client/src/App.js
+++ ContactApp/client/src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Имя контакта</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Имя фамилия 1</td>
+                <td>example@email1.ru</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Имя фамилия 2</td>
+                <td>example@email2.ru</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Имя фамилия 3</td>
+                <td>example@email3.ru</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
