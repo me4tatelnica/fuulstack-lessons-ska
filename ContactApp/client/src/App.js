76c051527e1dd55162323cf4b0b86bd680bb6fd9
me4tatelnica <mamiha2010@gmail.com>
2025-09-03 09:27:16 +0300

    Пройден урок "React-хуки в деле"

--- ContactApp/client/src/App.js
+++ ContactApp/client/src/App.js
@@ -1,22 +1,25 @@
+import React, { useState } from "react"
 import TableContact from "./layout/TableContact/TableContact";
 
-const contacts = [
-  { id: 1, name: 'Имя Фамилия 1', email: "Q@e1.rt"},
-  { id: 2, name: 'Имя Фамилия 2', email: "Q@e2.rt"},
-  { id: 3, name: 'Имя Фамилия 3', email: "Q@e3.rt"},
-];
-
-const addContact = () => {
-  const item = {
-    id: Math.floor(Math.random() * 100),
-    name: "Имя Фамилия 3",
-    email: "Q@e3.rt"
-  };
-  contacts.push(item);
-  console.log(contacts);
-}
-
 const App = () => {
+  const [contacts, setContacts] = useState(
+    [
+      { id: 1, name: "Имя Фамилия 1", email: "Q@e1.rt" },
+      { id: 2, name: "Имя Фамилия 2", email: "Q@e2.rt" },
+      { id: 3, name: "Имя Фамилия 3", email: "Q@e3.rt" },
+    ]
+);
+
+  const addContact = () => {
+    const item = {
+      id: Math.floor(Math.random() * 100),
+      name: "Имя Фамилия 3",
+      email: "Q@e3.rt",
+    };
+    setContacts([...contacts, item]);
+    console.log(contacts);
+  };
+
   return (
     <div className="container mt-5">
       <div className="card">
