917cf6deb4325435c1020241cba0350ab23d4e27
me4tatelnica <mamiha2010@gmail.com>
2025-09-05 07:16:40 +0300

    Пройден урок "Унификация ID"

--- ContactApp/client/src/App.js
+++ ContactApp/client/src/App.js
@@ -4,19 +4,24 @@ import TableContact from "./layout/TableContact/TableContact";
 const App = () => {
   const [contacts, setContacts] = useState(
     [
-      { id: 1, name: "Имя Фамилия 1", email: "Q@e1.rt" },
-      { id: 2, name: "Имя Фамилия 2", email: "Q@e2.rt" },
-      { id: 3, name: "Имя Фамилия 3", email: "Q@e3.rt" },
+      { id: 21, name: "Имя Фамилия 1", email: "Q@e1.rt" },
+      { id: 12, name: "Имя Фамилия 2", email: "Q@e2.rt" },
+      { id: 6, name: "Имя Фамилия 3", email: "Q@e3.rt" },
     ]
 );
 
   const addContact = () => {
+    let newId = Math.max(...contacts.map(e => e.id)) + 1;
+
+    console.log(newId);
+
     const item = {
-      id: Math.floor(Math.random() * 100),
+      id: newId,
       name: "Имя Фамилия 3",
       email: "Q@e3.rt",
     };
     setContacts([...contacts, item]);
+
     console.log(contacts);
   };
 
