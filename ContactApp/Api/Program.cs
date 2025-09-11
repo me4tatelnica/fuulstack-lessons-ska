173824b7e3798dc0d577a2c82407855283ab77cc
me4tatelnica <mamiha2010@gmail.com>
2025-09-11 09:29:19 +0300

    Пройден урок "Секретики"

--- ContactApp/Api/Program.cs
+++ ContactApp/Api/Program.cs
@@ -17,7 +17,7 @@ opt.AddPolicy("CorsPolicy", policy =>
 {
   policy.AllowAnyMethod()
   .AllowAnyHeader()
-  .WithOrigins("http://localhost:3000");
+  .WithOrigins(args[0]);
 }));
 
 var app = builder.Build();
